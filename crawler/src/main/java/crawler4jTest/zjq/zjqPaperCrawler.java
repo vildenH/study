86a74f30e7d8447b4cb8e9a3f5b1c3536ef09983
吴昊 <wuhao14@meituan.com>
2018-08-03 15:42:08 +0800

    add crawler

---
+++ crawler/src/main/java/crawler4jTest/zjq/zjqPaperCrawler.java
@@ -0,0 +1,13 @@
+package crawler4jTest.zjq;
+
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+
+/**
+ * @author wh
+ * @date 2018/8/3
+ */
+public class zjqPaperCrawler extends WebCrawler {
+
+
+
+}
