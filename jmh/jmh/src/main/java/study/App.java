188f1476f39d78b9cb38a005d975b41078982de3
吴昊 <wuhao14@meituan.com>
2018-09-12 21:45:24 +0800

    学习jmh

---
+++ jmh/jmh/src/main/java/study/App.java
@@ -0,0 +1,15 @@
+package study;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * Hello world!
+ */
+public class App {
+    @Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+
+}
