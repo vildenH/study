ef0b6f713246a58a4798900d4bba5f9da0c4061e
吴昊 <wuhao14@meituan.com>
2018-12-29 16:10:27 +0800

    learn fsm

--- designPattern/src/main/java/FSM/main.java
+++ designPattern/src/main/java/FSM/main.java
@@ -6,4 +6,32 @@ package FSM;
  */
 public class main {
 
+    enum State {
+        Initial(false),
+        Final(true),
+        Error(false);
+
+        static public final Integer length = 1 + Error.ordinal();
+
+        final boolean accepting;
+
+        State(boolean accepting) {
+            this.accepting = accepting;
+        }
+    }
+
+    enum Symbol {
+        A, B, C;
+        static public final Integer length = 1 + C.ordinal();
+    }
+
+    State transition[][] = {
+            //      A               B               C
+            {
+                    State.Initial,  State.Final,    State.Error
+            }, {
+                    State.Final,    State.Initial,  State.Error
+    }
+    };
 }
+
